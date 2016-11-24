456d16f7b5974c21eeb666822b5169ec35c40cd0
wangyunfei <wangyunfei@wangyunfei.local>
2016-11-24 20:07:04 +0800

    wangyunfei

--- hello.go
+++ hello.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+    "os"
+    "io"
+    "net/http"
+    "strconv"
+
+    "github.com/Jack47/hugger"
+    logging "github.com/op/go-logging"
+)
+
+func heartbreakerHandler(w http.ResponseWriter, req *http.Request) {
+    logger.Infof("Meet heartbreaker from %s", req.Host)
+    io.WriteString(w, hugger.Hug())
+}
+
+var LISTENING_PORT = 1024
+var logger = logging.MustGetLogger("hugmachine.log")
 
 func main() {
-	fmt.Printf("Hello, world.\n")
+    logging.NewLogBackend(os.Stderr, "", 0)
+    http.HandleFunc("/heartbreaker", heartbreakerHandler)
+    logger.Infof("Listening on port %d", LISTENING_PORT)
+    err := http.ListenAndServe("0.0.0.0:"+strconv.Itoa(LISTENING_PORT), nil)
+    if err != nil {
+        logger.Fatal("ListenAndServe: " + err.Error())
+    }
 }
+
